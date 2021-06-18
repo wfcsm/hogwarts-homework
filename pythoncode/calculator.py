e8110ffab36ca049fc35643329e044fdcb70932e
zyc <627245048@qq.com>
2021-06-18 15:09:07 +0800

    6.18

--- pythoncode/calculator.py
+++ pythoncode/calculator.py
@@ -2,6 +2,7 @@
 __author__ = 'hogwarts_xixi'
 __time__ = '2021/6/17 8:26 下午'
 """
+import decimal
 
 # 被测代码 计算器
 class Calculator:
@@ -12,7 +13,9 @@ class Calculator:
         return a - b
 
     def mul(self, a, b):
-        return a * b
+        return round(a * b,5)
 
     def div(self, a, b):
+        if b == 0:
+            return "除数不能为0"
         return a / b
--- testing/test_calculator.py
+++ testing/test_calculator.py
@@ -7,35 +7,39 @@ import logging
 import pytest
 import yaml
 
+
 from pythoncode.calculator import Calculator
 
+
 # 获取 测试数据
-def get_calc_data():
-    with open('./datas/calc.yml') as f:
+def get_calc_data(data_url):
+    with open(file=data_url) as f:
         totals = yaml.safe_load(f)
-    return (totals['datas'],totals['ids'])
+    return (totals['datas'], totals['ids'])
+
 
 # 测试 获取数据的方法
 def test_getdatas():
     print(get_calc_data())
 
 
-
 class TestCalculator:
     # setup teardown 每条用例前后分别被调用
     # setup_class teardown_class 在类执行的前后分别被调用一次
-    def setup_class(self):
+    def setup(self):
         print("开始计算")
         logging.info("开始计算")
         # 加了self 之后，calc就变成了实例变量，就可以在其它的用例当中调用了
         self.calc = Calculator()
 
-    def teardown_class(self):
+    def teardown(self):
         print("结束计算")
 
+    # 加法
     @pytest.mark.add
-    @pytest.mark.parametrize('a,b,expect',get_calc_data()[0],ids=get_calc_data()[1])
-    def test_add(self,a,b,expect):
+    @pytest.mark.parametrize('a,b,expect', get_calc_data(data_url='./datas/calc.yml')[0],
+                             ids=get_calc_data(data_url='./datas/calc.yml')[1])
+    def test_add(self, a, b, expect):
         # calc = Calculator()
         assert expect == self.calc.add(a, b)
 
@@ -44,7 +48,23 @@ class TestCalculator:
     #     # calc = Calculator()
     #     assert 2000 == self.calc.add(1000, 1000)
 
+    # 除法
     @pytest.mark.div
-    def test_div(self):
+    @pytest.mark.parametrize("a,b,expect", argvalues=get_calc_data(data_url='./datas/calc_div.yml')[0],
+                             ids=get_calc_data(data_url='./datas/calc_div.yml')[1])
+    def test_div(self, a, b, expect):
         # calc = Calculator()
-        assert 1 == self.calc.div(1, 1)
+        assert expect == self.calc.div(a, b)
+
+    # 减法
+    @pytest.mark.parametrize("a,b,expect",argvalues=get_calc_data(data_url='./datas/calc_sub.yml')[0],
+                             ids=get_calc_data(data_url='./datas/calc_sub.yml')[1])
+    def test_sub(self,a,b,expect):
+        assert expect == self.calc.sub(a,b)
+
+
+    # 乘法
+    @pytest.mark.parametrize("a,b,expect",argvalues=get_calc_data(data_url='./datas/calc_mul.yml')[0],
+                             ids=get_calc_data(data_url='./datas/calc_mul.yml')[1])
+    def test_mul(self,a,b,expect):
+        assert expect == self.calc.mul(a,b)
